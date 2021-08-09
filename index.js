c8a20ab64ac660c2ba57e54f719f4cc7b247bf42
roger chat <mattrogerpelosi@gmail.com>
2021-08-09 18:05:55 -0400

    commit tree 820bfeedd6da7a35bec7d88fc6f1d44caa5b7968

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+   return pickupBlock > 42 ? (pickupBlock-42) : (42-pickupBlock);
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+    return (distanceFromHqInBlocks(pickupBlock)) * 264;
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    if(num1 > num2) {
+        return (num1-num2)*264;
+    } else {
+        return (num2-num1)*264;
+    }
+}
+
+function calculatesFarePrice(num1, num2) {
+    if(distanceTravelledInFeet(num1, num2) <= 400) {
+        return 0;
+    } else if((distanceTravelledInFeet(num1, num2)) <= 2000) {
+        let dist = distanceTravelledInFeet(num1, num2);
+        return (dist-400)*(.02);
+    } else if(distanceTravelledInFeet(num1, num2) <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+
+}
